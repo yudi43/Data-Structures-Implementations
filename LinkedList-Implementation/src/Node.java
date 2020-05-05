096af4c6e9d996bf43801e8fba98abefead01f26
Yudhishthir Singh <yudhisthir.singh.rajawat@joulon.com>
2020-05-05 11:02:31 +0530

    Added node class

---
+++ LinkedList-Implementation/src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    int data; //Data in the current node
+    Node next; //Reference for the next node
+
+    //Constructor which takes an int value which is stored as the data in this Node object.
+    Node(int data) {
+        this.data = data;
+    }
+}
