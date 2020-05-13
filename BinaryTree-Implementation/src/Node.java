57b19641f55fda491722175ca5b9a961a4c73f4e
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-05-13 17:30:45 +0530

    Added function to add items to the binary tree

--- BinaryTree-Implementation/src/Node.java
+++ BinaryTree-Implementation/src/Node.java
@@ -2,13 +2,34 @@
  * @author Yudhishthir Singh. Bangalore. 2020
  */
 
-
+//This file contains the full implementation of the Binary Tree, a Node must be created using the constructor and operations can be performed on the created node.
 public class Node {
     Node left;
     Node right;
     int data;
 
+    //Constructor for created the first item of the binary tree, and this node represents the value of field 'data'
     Node(int data) {
         this.data = data;
     }
+
+    //function to insert items in the tree:
+    //The function takes a value which the new node will represent
+    public void insert(int value) {
+        if(value <= data) { //if the new value is lesser than the root, it goes to the left of it...
+            if(left == null) {
+                left = new Node(value);
+            }else{ //if there are items on the left of the root, make recursive calls until you find null
+                left.insert(value);
+            }
+        } else { //the value is greater than the variable data
+            if(right == null) {
+                right = new Node(value);
+            }else{
+                right.insert(value);
+            }
+        }
+    }
+
 }
+
