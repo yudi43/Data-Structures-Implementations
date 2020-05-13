4c6be4e34ac5dcc3255bf75646edea4fbe51ec0e
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-05-13 15:20:36 +0530

    Initial commit for binary tree implementation

---
+++ BinaryTree-Implementation/src/Node.java
@@ -0,0 +1,12 @@
+/**
+ * @author Yudhishthir Singh. Bangalore. 2020
+ */
+public class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
